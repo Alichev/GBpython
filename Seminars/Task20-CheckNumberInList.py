9e86cd1edde299e2fffe709fd397e41359be95a7
Anton Alichev <tosiakz@yandex.ru>
2022-12-26 20:39:25 +0300

    add seminar 3 tasks

---
+++ Seminars/Task20-CheckNumberInList.py
@@ -0,0 +1,21 @@
+# Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число
+
+list_num = [1, 8, 'hello', 25, 'привет']
+# find = input('Введите искомое значение: ') #для строки
+# for i in range(len(list_num)):
+#     if list_num[i] == find:
+#         print('Искомое значение находится на позиции ', i)
+#         break
+# else:
+#     print('Искомое значение не найдено')
+
+find = int(input('Введите искомое число: '))
+if find in list_num:
+    print('Да, позиция ', list_num.index(find))
+else:
+    print('Нет')
+
+
+# for i in list_num:
+#     if i.isdigit():
+#         print(list_num.index(i))
